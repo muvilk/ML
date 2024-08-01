46d08641fe3d2af06fe0d7723e6da47743ca474d
Yongster1000 <161622849+Yongster1000@users.noreply.github.com>
2024-08-02 00:07:38 +0800

    yes

--- Task 3/final model.py
+++ Task 3/final model.py
@@ -4,23 +4,23 @@ import numpy as np
 test = pd.read_csv("Data/test_tfidf_features.csv")
 
 # Read the first set of predictions
-y_pred1 = pd.read_csv("Task3_predictions/Optimal_LogReg.csv")
+y_pred1 = pd.read_csv("Data/Optimal_LogReg.csv")
 y_pred1 = y_pred1['label'].values
 
 # Read the second set of predictions
-y_pred2 = pd.read_csv("Task3_predictions/Optimal_gbdt.csv")
+y_pred2 = pd.read_csv("Data/Optimal_gbdt.csv")
 y_pred2 = y_pred2['label'].values
 
 # Read the third set of predictions
-y_pred3 = pd.read_csv("Task3_predictions/Optimal_forest.csv")
+y_pred3 = pd.read_csv("Data/Optimal_forest.csv")
 y_pred3 = y_pred3['label'].values
 
 # Read the fourth set of predictions
-y_pred4 = pd.read_csv("Task3_predictions/Optimal_NB.csv")
+y_pred4 = pd.read_csv("Data/Optimal_NB.csv")
 y_pred4 = y_pred4['label'].values
 
 # Read the fifth set of predictions
-y_pred5 = pd.read_csv("Task3_predictions/Optimal_svm.csv")
+y_pred5 = pd.read_csv("Data/Optimal_svm.csv")
 y_pred5 = y_pred5['label'].values
 
 # Combine predictions into a DataFrame for easier manipulation
@@ -41,4 +41,4 @@ print(predictions_df.head())
 
 # Save the final predictions to a CSV file
 final_df = predictions_df[['id', 'label']]
-final_df.to_csv('final_predictions.csv', index=False)
+final_df.to_csv('BestModel_Predictions.csv', index=False)
